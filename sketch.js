ef41086797000400d515d49f508f3bec110af2a3
AndreaSanguineti <90753738+AndreaSanguineti@users.noreply.github.com>
2021-11-09 23:16:17 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,69 @@
+const phrase =
+  "Phil Claire Haley Alex Luke Jay Gloria Manny Joe Mitchell Cam Lily Stella";
+const words = phrase.split(" ");
+let allButtons;
+let iterator = 0;
+let modernFamilyImg;
+let intro;
+
 function preload() {
-  // put preload code here
+  modernFamily = loadImage("./assets/modernfamily.jpg");
+  intro = loadSound("./assets/intro.mp4");
+}
+
+//adattare l'animazione alla finestra
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  // the same as writing: function (variable) {}
+  words.forEach((word) => {
+    createElement("button", word);
+  });
+
+  allButtons = selectAll("button");
+
+  console.log(allButtons);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  //carico l'immagine
+  modernFamilyImg = image(modernFamily, 0, 0, windowWidth, windowHeight);
+
+  //testo 1
+  textAlign(CENTER);
+  fill("#FE5D9F");
+  strokeWeight(3);
+  textSize(36);
+  text("Find the intruder", windowWidth / 2, windowHeight / 9);
+
+  //testo 2
+  textAlign(CENTER);
+  fill("#FE5D9F");
+  strokeWeight(3);
+  textSize(16);
+  text("(and click to let the intro begin)", windowWidth / 2, windowHeight / 7);
+
+  //muovo i pulsanti
+  iterator++;
+  allButtons.forEach((singleButton, i) => {
+    let x = noise((iterator + 40 * i) / 400) * windowWidth;
+    let y = noise((iterator - 40 * i) / 400) * windowHeight;
+
+    singleButton.style("position: absolute; background-color: #FE5D9F");
+    singleButton.position(x, y);
+  });
+}
+
+//metto la musica
+function mouseClicked() {
+  if (intro.isPlaying()) {
+    intro.pause();
+    play = false;
+  } else {
+    intro.play();
+    play = true;
+  }
 }
